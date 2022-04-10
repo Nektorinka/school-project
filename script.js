8cc33ba4ae63811315aa3be1902083bdb13cc1b0
knyazhe <evgeniyknyaz435@gmail.com>
2022-04-10 14:32:38 +0300

    base64 + const
    base64 encode and answers are a constant

--- script.js
+++ script.js
@@ -4,39 +4,41 @@ function getRandom(min, max, d = 10) {
 	return Math.floor(Math.random() * (max - min)*d)/d + min;
 }
 
-var responses = [];
+var tempResponses = [];
 var examples = []; 
 var q_examples = 0;
 var i_examples = 0;
 
 function generateExamples(operator, q = 5, dig1 = 20, dig2 = 20){
 	for (var i = 1; i <= q; ++i){
-		console.log(i_examples, q);
 		var d1 = getRandom(1, dig1);
 		var d2 = getRandom(1, dig2);
 		if (operator == '+'){
-			var d3 = (d1 + d2).toFixed(1);
+			var d3 = d1 + d2;
 		} else if(operator == "-"){
-			console.log("minus");
 			d3 = Math.min(d1, d2);
 			d1 = Math.max(d1, d2);
 			d2 = d3;
-			var d3 = (d1 - d2).toFixed(1);
+			var d3 = d1 - d2;
 		}else if(operator == "*"){
 			d2 = Math.floor(d2);
-			var d3 = (d1*d2).toFixed(1);
+			var d3 = d1*d2;
 		}else if(operator == "/"){
 			var d3 = d1;
 			var d2 = Math.floor(d2);
 			var d1 = (d3 * d2).toFixed(1);
 		}
-		responses.push(d3);
+		tempResponses.push(btoa(d3.toFixed(1)));
 		examples.push(`${d1} ${operator} ${d2} = `);
 		let example = document.getElementById("example" + String(i_examples + i));
 		example.textContent = `${d1} ${operator} ${d2} = `;
 	}
 	i_examples += (--i);
 	q_examples += q;
+}
+
+function glob(){
+	return tempResponses;
 }
 
 function sendForm(e){
@@ -55,7 +57,7 @@ function sendForm(e){
 		if (val !== ""){
 			val = Number(val).toFixed(1);
 		}
-		if (val == Number(responses[i-1])){
+		if (val == Number(atob(responses[i-1]))){
 	    		correct++;
 		}
 		let tr = document.createElement('tr');
@@ -66,7 +68,7 @@ function sendForm(e){
 		tr.append(td3);
 		td1.innerHTML = val;
 		tr.append(td1);
-		td2.innerHTML = responses[i-1];
+		td2.innerHTML = atob(responses[i-1]);
 		tr.append(td2);
 		table.append(tr);
 
